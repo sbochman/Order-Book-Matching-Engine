87f97db492c8c73e304e5de13fe7e3ee4f9a1e08
sbochman <108441256+sbochman@users.noreply.github.com>
2025-06-08 10:39:24 -0700

    init commit of basic Order and OrderBook class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "OrderBook.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //change branch to main
+    OrderBook ob;
+    ob.add_order(Order(101.0, false)); // Buy order at 101
+    ob.add_order(Order(99.0, true));   // Sell order at 99
+
+    Order best_buy = ob.get_best_buy_order();
+    Order best_sell = ob.get_best_sell_order();
+
+    std::cout << "Best buy: " << best_buy.get_value() << "\n";
+    std::cout << "Best sell: " << best_sell.get_value() << "\n";
+
     return 0;
 }
